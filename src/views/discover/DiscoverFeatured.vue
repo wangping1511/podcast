931c4a51808e28bf6eecffdf5560a066bbe5a294
wangping <wangping1511@outlook.com>
2022-01-17 23:38:47 +0800

    update

--- src/views/discover/DiscoverFeatured.vue
+++ src/views/discover/DiscoverFeatured.vue
@@ -2,7 +2,7 @@
   <div class="mb-5">
     <div class="flex dark:text-white overflow-x-scroll scrollbar-hide">
       <div v-for="(item, index) in list"
-           :class="(index === 0 ? 'ml-5' : '' ) + (index === list.length -1 ? ' mr-[1.2rem]' : ' mr-3')"
+           :class="(index === 0 ? 'ml-5' : '' ) + (index === list.length -1 ? '' : ' mr-3')"
            class="w-[89.8%] flex-none flex flex-col justify-between">
         <div>
           <p class="text-xs font-medium text-primary">精选</p>
@@ -10,6 +10,7 @@
         </div>
         <div class="mt-1 rounded bg-primary w-full h-42"></div>
       </div>
+      <div class="flex-none w-[1.2rem]"></div>
     </div>
   </div>
 </template>
