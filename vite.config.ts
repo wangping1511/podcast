f9d06a34645dea80b68243af5b118820b3946ff4
wangping <wangping1511@outlook.com>
2022-01-19 21:36:09 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import Components from 'unplugin-vue-components/vite'
 import WindiCSS from 'vite-plugin-windicss'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -29,7 +30,8 @@ export default defineConfig({
         IconsResolver()
       ]
     }),
-    Icons({})
+    Icons({}),
+    viteCompression()
   ],
   resolve: {
     // 设置别名
